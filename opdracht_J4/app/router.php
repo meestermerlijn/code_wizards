8224b1a17688ec93058097df26fe5de67bffdbd6
Menno <menno@mmerlijn.nl>
2024-11-06 23:26:41 +0100

    opd j

--- opdracht_J4/app/router.php
+++ opdracht_J4/app/router.php
@@ -18,13 +18,6 @@ $route->post('login', "controllers/login.php");
 $route->get('logout', "controllers/logout.php");
 
 
-$route->get('items/{id}', 'controllers/items-show.php');
-$route->get('items', 'controllers/items-index.php');
-$route->get('items/create', 'controllers/items-create.php');
-$route->post('items', 'controllers/items-store.php');
-$route->get('items/{id}/edit', 'controllers/items-edit.php');
-$route->put('items/{id}', 'controllers/items-update.php');
-$route->delete('items/{id}', 'controllers/items-destroy.php');
 //Alleen als je ingelogd bent
 if (auth()) {
     //hier komen routes die je alleen kan bereiken als je ingelogd bent
---
+++ opdracht_X_dev/app/views/posts-create.view.php
@@ -0,0 +1,20 @@
+<?php
+view("parts/header", ['title' => 'Nieuwe post aanmaken']);
+view("parts/navigatie-menu");
+?>
+    <div class="sm:mx-10">
+        <h1 class="text-3xl my-4">Nieuwe post maken</h1>
+        <form action="/posts-store" method="post">
+            <?= csrf() ?>
+            <input type="text" name="title" placeholder="Titel" required><br>
+            <textarea name="content" placeholder="Content..."></textarea><br>
+            <input type="submit" value="Opslaan" class="border border-1 rounded-md px-2 py-1 hover:bg-gray-100 cursor-pointer">
+        </form>
+
+    </div>
+
+
+<?php
+view("parts/footer");
+
+
